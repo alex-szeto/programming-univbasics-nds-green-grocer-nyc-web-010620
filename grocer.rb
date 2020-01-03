584273adab297582bd021603c93f64784fae684e
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 13:31:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,12 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  collection.each{ |element|
+    if element[:item] == name
+      return element
+    end
+  }
+  nil
 end
 
 def consolidate_cart(cart)
@@ -9,6 +15,20 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  result = []
+  cart.each { |element|
+    if find_item_by_name_in_collection(element[:item], result)
+      find_item_by_name_in_collection(element[:item], result)[:count] += 1
+    else
+      result << {
+        :item => element[:item],
+        :price => element[:price],
+        :clearance => element[:clearance],
+        :count => 1
+      }
+    end
+  }
+  return result
 end
 
 def apply_coupons(cart, coupons)
@@ -21,6 +41,7 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
 end
 
 def checkout(cart, coupons)
