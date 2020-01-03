912d60b003e8fe2ae92f6132e0183d6c27f6fe31
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 13:32:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
   coupons.each{ |element|
     element = coupon[:item]
-    item = 
+    item = find_item_by_name_in_collection()
   }
   return cart
 end
